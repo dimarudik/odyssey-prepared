4e4c8d573a8b83df5153d045d9ffc6cccd9545ef
Dmitrii Rudik <dima.rudik@gmail.com>
2023-05-07 12:41:07 +0300

    Docker odyssey

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,26 +11,25 @@ public class App {
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection(args[0])) {
             connection.setAutoCommit(false);
-//            String selectSQL = "select pg_backend_pid() where 1 = ?";
             String selectSQL = "select id from test where id = ?";
 
-            int n = 10000;
-            int backend = 0;
+            int n = 20000;
+            int id = 0;
             long start = System.currentTimeMillis();
             for (int i = 0; i < n; i++) {
                 PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
                 preparedStatement.setInt(1, 1);
                 ResultSet rs = preparedStatement.executeQuery();
                 if (rs.next()) {
-                    backend = rs.getInt(1);
+                    id = rs.getInt(1);
                 }
                 connection.commit();
             }
             long finish = System.currentTimeMillis();
 
 
-            logger.info("pid = {}; time (sec) = {}; executions per second = {}",
-                    backend, (finish - start) / 1000d, (n * 1d / ((finish - start) / 1000)));
+            logger.info("\t\t executions per second = {};\t\t elapsed time (sec) = {}; ",
+                    (n * 1d / ((finish - start) / 1000)), (finish - start) / 1000d );
         } catch (SQLException e) {
             logger.error("{}", e);
         }
