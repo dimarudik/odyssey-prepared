de85a5b6523fc2b9edb958870fb48a91a933281d
Dmitrii Rudik <dima.rudik@gmail.com>
2023-11-13 12:37:28 +0300

    123

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.postgresql.PGConnection;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -15,14 +16,16 @@ public class App {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         String url = args[0];
         int threads = Integer.parseInt(args[1]);
+        int lifeTime = Integer.parseInt(args[2]);
         ExecutorService executorService = Executors.newFixedThreadPool(threads + 1);
         List<Future<LogMessage>> tasks = new ArrayList<>();
         for (int i = 0; i < threads; i++) {
-            tasks.add(executorService.submit(new CallableTask(url)));
+            tasks.add(executorService.submit(new CallableTask(url, lifeTime)));
             Thread.sleep(10);
         }
 
         Iterator<Future<LogMessage>> futureIterator = tasks.listIterator();
+        double elapsedTime = 0;
         while (futureIterator.hasNext()) {
             Future<LogMessage> future = futureIterator.next();
             if (future.isDone()) {
@@ -32,6 +35,8 @@ public class App {
                             logMessage.threadName(), logMessage.backend(), logMessage.elapsedTime());
                 }
                 futureIterator.remove();
+                assert logMessage != null;
+                elapsedTime += logMessage.elapsedTime();
             }
             if (!futureIterator.hasNext()) {
                 futureIterator = tasks.listIterator();
@@ -40,41 +45,35 @@ public class App {
         }
 
         executorService.shutdown();
+        System.out.println(elapsedTime / threads);
     }
 }
 
 class CallableTask implements Callable<LogMessage> {
     private final String url;
+    private final int lifeTime;
     private static final Logger logger = LogManager.getLogger(CallableTask.class);
 
-    public CallableTask(String url) {
+    public CallableTask(String url, int lifeTime) {
         this.url = url;
+        this.lifeTime = lifeTime;
     }
 
     @Override
     public LogMessage call() {
-        LogMessage logMessage = null;
+        LogMessage logMessage;
         long start = System.currentTimeMillis();
-        try (Connection connection = DriverManager.getConnection(url);
-             Statement statement = connection.createStatement()) {
-            int id = 0;
+        try (Connection connection = DriverManager.getConnection(url)) {
             long finish = System.currentTimeMillis();
-
-            String selectSQL = "select pg_backend_pid()";
-            ResultSet resultSet = statement.executeQuery(selectSQL);
-            if (resultSet.next()) {
-                id = resultSet.getInt(1);
-            }
-            resultSet.close();
-
-            logMessage = new LogMessage(Thread.currentThread().getName(), id, (finish - start) / 1000d);
-            Thread.sleep(1000);
+            PGConnection pgConnection = connection.unwrap(PGConnection.class);
+            logMessage = new LogMessage(Thread.currentThread().getName(), pgConnection.getBackendPID(), (finish - start) / 1000d);
+            Thread.sleep(lifeTime);
         } catch (SQLException | InterruptedException e) {
+            logMessage = new LogMessage(Thread.currentThread().getName(), -1, 0);
             logger.error("{}", e);
         }
         return logMessage;
     }
-
 }
 
 record LogMessage (String threadName, int backend, double elapsedTime) {}
